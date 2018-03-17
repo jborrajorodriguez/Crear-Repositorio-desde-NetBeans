b532a8e64626af7a66a99c188d10f4dda815fe06
Juan Borrajo Rodriguez <juanborrajo10@gmail.com>
2018-03-17 12:14:27 +0100

    Metodos para crear un repositorio publico

--- src/main/java/com/mycompany/crearrepositorioengithub/Principal.java
+++ src/main/java/com/mycompany/crearrepositorioengithub/Principal.java
@@ -1,5 +1,11 @@
 package com.mycompany.crearrepositorioengithub;
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
+
 /**
  *
  * @author Juan Borrajo
@@ -7,6 +13,14 @@ package com.mycompany.crearrepositorioengithub;
 public class Principal {
 
     public static void main(String[] args) {
+        try {
+            GitHub github = GitHub.connect();
+            GHRepository repo = github.createRepository(
+                    "Repo creado desde netbeans","Repositorio de prueba de la API de GitHub",
+                    " ",true/*public*/);
+        } catch (IOException ex) {
+            Logger.getLogger(Principal.class.getName()).log(Level.SEVERE, null, ex);
+        }
         
     }
     
